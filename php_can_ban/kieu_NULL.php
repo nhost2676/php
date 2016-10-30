0528d4a85809db1a468856762aa6b78f815a1fb0
nhost2676 <tokarinneji@gmail.com>
2016-10-30 16:41:35 +0700

    Update kieu_NULL.php

--- php_can_ban/kieu_NULL.php
+++ php_can_ban/kieu_NULL.php
@@ -7,8 +7,8 @@
 	<?php
 	/* 
 	Đây là kiểu đặc biệt trong PHP và cũng như các ngôn ngữ lập trình khác, nó mang giá trị rỗng.
-    Lúc bạn khởi tạo một biến và bạn gán = NULL thì sẽ hệ thông sẽ không tốn bộ nhớ để lưu trữ, nên việc sử dụng nó rất có lợi.         
-    Kiểu NULL khi ép kiểu sang kiểu INT thì bằng 0, khi ép kiểu sang kiểu chuỗi thì = rỗng, và khi ép sang kiểu boolean thì mang giá trị FALSE.
+    	Lúc bạn khởi tạo một biến và bạn gán = NULL thì sẽ hệ thông sẽ không tốn bộ nhớ để lưu trữ, nên việc sử dụng nó rất có lợi.         
+    	Kiểu NULL khi ép kiểu sang kiểu INT thì bằng 0, khi ép kiểu sang kiểu chuỗi thì = rỗng, và khi ép sang kiểu boolean thì mang giá trị FALSE.
 	*/
 	$a = NULL;
 	var_dump($b = (int)$a);
