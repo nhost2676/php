2ba9d6c35ffc68b7a0a33fc30a085d8d37326001
Nhat Nam <tokarinneji@gmail.com>
2016-11-08 19:11:24 +0700

    PHP
    My code

--- php_can_ban/de_quy.php
+++ php_can_ban/de_quy.php
@@ -37,5 +37,29 @@ function pheptinh($n){
 		return $tong;
 	}
 }
-echo pheptinh(6)
-?>
+echo pheptinh(6);
+echo "<br>\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/</br>";
+/* 4. Đệ quy hổ tương
+ Đệ quy hổ tương là đệ quy một hàm A gọi sang một hàm B, Trong hàm B lại gọi sang hàm A. Như vậy là chúng gọi lẫn nhau nên người ta gọi là  hổ tương.
+ Cũng như các loại đệ quy trên kia, nếu cả 2 hàm A, B đều không có điều kiện dừng thì sẽ bị lặp vô hạn, điều này rất nguy hiểm nên các bạn phải chú ý.*/
+//Hàm đệ quy U
+ function U($n){
+	if ($n < 5){ // ĐK DỪNG
+		return $n;
+	}
+	else{
+		return U($n-1) + U($n - 2);
+	}	
+}
+//Hàm đệ quy G
+ function G($n){
+	if ($n <= 8){ // ĐK DỪNG
+		return $n - 3;
+	}
+	else{
+		return U($n-1) + G($n - 2);
+	}	
+}
+// Goi ham
+echo G(12)
+ ?>
