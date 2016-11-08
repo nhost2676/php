94a55e47f402f02d2f8962e714f48f1472314dee
Nhat Nam <tokarinneji@gmail.com>
2016-11-08 19:04:07 +0700

    php
    my code

--- php_can_ban/de_quy.php
+++ php_can_ban/de_quy.php
@@ -20,5 +20,22 @@ function Fibo($n)
 	}
 }
 echo Fibo(20);
-
+echo "<br>\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/</br>";
+/* 3. Đệ quy phi tuyến
+Là loại đệ quy mà trong hàm có dùng vòng lặp để gọi lại chính nó.
+*/
+function pheptinh($n){
+	// Nếu $n < 6 thì trả về chính nó
+	if ($n < 6){
+		return $n;
+}
+	else{
+		$tong = 0;
+		for ($i = 1; $i < $n; $i++){
+			$tong += pheptinh($n - $i);
+			}
+		return $tong;
+	}
+}
+echo pheptinh(6)
 ?>
